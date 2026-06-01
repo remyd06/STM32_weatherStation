78ecc10b2df22c16a44636e7477c87e7117546dd
Rémy Dedola <158462966+remyd06@users.noreply.github.com>
2026-06-01 18:29:26 +0200

    Add the modular SPI transmit/receive

--- BSP/SPI/spi.c
+++ BSP/SPI/spi.c
@@ -9,3 +9,20 @@ void	SPI1_Init()
 	SPI1->CR1 |= (1 << 8);
 	SPI1->CR1 |= (1 << 6);
 }
+
+void	SPI_Transmit(SPI_TypeDef *SPIx, uint8_t *data)
+{
+	while (!(SPIx->SR & (1 << 1)));
+	SPIx->DR = *data;
+	while (SPIx->SR & (1 << 7));
+}
+
+uint8_t	SPI_Receive(SPI_TypeDef *SPIx)
+{
+	uint8_t	data;
+
+	while (!(SPIx->SR & (1 << 0)));
+	data = SPIx->DR;
+	while (SPIx->SR & (1 << 7));
+	return (data);
+}
--- BSP/SPI/spi.h
+++ BSP/SPI/spi.h
@@ -1,6 +1,12 @@
 #ifndef SPI_H
 #define SPI_H
 
+# include <stdint.h>
+# include "stm32f446xx.h"
+# include "gpio.h"
+
 void	SPI1_Init();
+void	SPI_Transmit(SPI_TypeDef *SPIx, uint8_t *data);
+uint8_t	SPI_Receive(SPI_TypeDef *SPIx);
 
 #endif
