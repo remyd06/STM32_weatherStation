9af89396f768911cb49aa9bc186270eb727e9db1
Rémy Dedola <158462966+remyd06@users.noreply.github.com>
2026-07-04 10:26:21 +0200

    Add I2C

--- BSP/RCC/rcc.c
+++ BSP/RCC/rcc.c
@@ -4,15 +4,14 @@ void	RCC_Init()
 {
 	//GPIO
 	RCC->AHB1ENR |= (1 << 0);	//GPIOA
+	RCC->AHB1ENR |= (1 << 1);	//GPIOB
 	RCC->AHB1ENR |= (1 << 2);	//GPIOC
 
-	//USART
-	RCC->APB1ENR |= (1 << 17);	//USART2
-
 	//PERIPHERALS
-	RCC->APB2ENR |= (1 << 12); //SPI1
-	RCC->AHB1ENR |= (1 << 22); //DMA2
-	RCC->APB2ENR |= (1 << 8);  //ADC1
-
+	RCC->APB1ENR |= (1 << 17);	//USART2
+	RCC->APB2ENR |= (1 << 12);	//SPI1
+	RCC->AHB1ENR |= (1 << 22);	//DMA2
+	RCC->APB2ENR |= (1 << 8); 	//ADC1
+	RCC->APB1ENR |= (1 << 21);	//I2C1
 }
 
