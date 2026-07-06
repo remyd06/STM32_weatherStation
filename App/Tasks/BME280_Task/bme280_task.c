39bd644ef1ae8d5ab272371127a569acb6314731
Rémy Dedola <158462966+remyd06@users.noreply.github.com>
2026-07-06 13:13:56 +0200

    Add bme280 task

---
+++ App/Tasks/BME280_Task/bme280_task.c
@@ -0,0 +1,56 @@
+#include "bme280_task.h"
+
+void	BME280_Task()
+{
+	uint8_t			id;
+	uint8_t			data[8];
+	char			buf[MSG_SIZE];
+
+	int32_t			int_part;
+	int32_t			dec_part;
+
+	BME280_calib_t	calib;
+	BME280_data_t	data_struct;
+
+	BME280_GetID(&id);
+	snprintf(buf, sizeof(buf), "!LOGS!ID: 0x%02X\r\n", id);
+//	xQueueSend(xQueueUart, buf, portMAX_DELAY);
+//	if (id == 0x60)
+//	{
+//		snprintf(buf, sizeof(buf), "!LOGS!BME280 detected.\r\n");
+//		xQueueSend(xQueueUart, buf, portMAX_DELAY);
+//	}
+//	else
+//	{
+//		snprintf(buf, sizeof(buf), "!LOGS!BME280 NOT detected.\r\n");
+//		xQueueSend(xQueueUart, buf, portMAX_DELAY);
+//	}
+
+	BME280_GetTRIM(&calib);
+	BME280_SetMODE(0b11, 0b001, 0b001, 0b001);
+
+	for (;;)
+	{
+		BME280_GetDATA(data);
+		BME280_ConvertDATA(data, &calib, &data_struct);
+
+		int_part = data_struct.temp / 100;
+		dec_part = data_struct.temp % 100;
+		snprintf(buf, sizeof(buf), "T:%ld.%ld\r\n", int_part, dec_part);
+//		xQueueSend(xQueueUart, buf, portMAX_DELAY);
+		xQueueSend(xQueueOled, buf, portMAX_DELAY);
+
+		int_part = (data_struct.pres / 256) /100;
+		snprintf(buf, sizeof(buf), "P:%ld\r\n", int_part);
+//		xQueueSend(xQueueUart, buf, portMAX_DELAY);
+		xQueueSend(xQueueOled, buf, portMAX_DELAY);
+
+		int_part = data_struct.hum / 1000;
+		dec_part = (data_struct.hum % 1000) / 10;
+		snprintf(buf, sizeof(buf), "H:%ld.%ld\r\n", int_part, dec_part);
+//		xQueueSend(xQueueUart, buf, portMAX_DELAY);
+		xQueueSend(xQueueOled, buf, portMAX_DELAY);
+
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
---
+++ App/Tasks/BME280_Task/bme280_task.h
@@ -0,0 +1,9 @@
+#ifndef BME280_TASK_H
+#define BME280_TASK_H
+
+# include "globals.h"
+# include "bme280.h"
+
+void	BME280_Task();
+
+#endif
