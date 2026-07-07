99a7bab727f463f35c434262ad8fa0acb8109c86
Rémy Dedola <158462966+remyd06@users.noreply.github.com>
2026-07-07 09:23:32 +0200

    Add alive var for watchdog

--- Core/Inc/globals.h
+++ Core/Inc/globals.h
@@ -5,10 +5,16 @@
 # include <stdio.h>
 # include "FreeRTOS.h"
 # include "queue.h"
+# include "iwdg_task.h"
 
 #define	MSG_SIZE	64
 
 extern QueueHandle_t	xQueueUart;
 extern QueueHandle_t	xQueueOled;
+
+extern volatile uint8_t	BME280_task_alive;
+extern volatile uint8_t	TEMT6000_task_alive;
+extern volatile uint8_t	SSD1306_task_alive;
+extern volatile uint8_t	UART_task_alive;
 
 #endif
--- Core/Src/globals.c
+++ Core/Src/globals.c
@@ -3,3 +3,8 @@
 
 QueueHandle_t	xQueueUart;
 QueueHandle_t	xQueueOled;
+
+volatile uint8_t BME280_task_alive = 0;
+volatile uint8_t TEMT6000_task_alive = 0;
+volatile uint8_t SSD1306_task_alive = 0;
+volatile uint8_t UART_task_alive = 0;
